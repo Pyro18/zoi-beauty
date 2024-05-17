6a20ddd9c9ae720d0b47cde0900a7e9c21cd7fec
Pyro18 <marius.noroaca@gmail.com>
2024-05-17 09:47:43 +0200

    update pagina home

--- server/frontend/components/userpages/homepage/about.php
+++ server/frontend/components/userpages/homepage/about.php
@@ -5,15 +5,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="/frontend/assets/css/homepage/home-about.css">
-</head>
-<body>
 
+    <style>
+        .welcome {
+            background-color: white;
+        }
+    </style>
+
+</head>
+
+
+<body>
+<div class="welcome">
 <div class="container" id="about">
     <div class="row">
         <div class="col">
             <section class="section">
-                <div class="meta">Welcome to</div>
-                <h2 class="name">[ ZOI - BEAUTY ]</h2>
+                <div class="meta">Benvenuti Su</div>
+                <h2 class="name">[ ZOI-BEAUTY.IT ]</h2>
                 <p class="description">Vestibulum ut mauris euismod, tristique augue sed, consequat metus. Duis fermentum massa ac metus suscipit tincidunt. Praesent felis felis, pretium sit amet vehicula at, posuere at mauris.</p>
             </section>
         </div>
@@ -56,6 +65,8 @@
 </div>
 </br>
 </br>
+</div>
+
 
 <script src="https://unpkg.com/@popperjs/core@2/dist/umd/popper.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
